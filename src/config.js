35e2ac486660f51454a0a28c1f05d0ff8e2e6cbb
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-04-11 22:49:51 -0600

    HRS-1 #comment Implementation of Signup backend and fixing some UI validation #time 1h 30m

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line no-undef
 export const API_URL = `https://backend-hotel-reservation-sys-d9bcaec0a6ce.herokuapp.com/api`;
+export const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
